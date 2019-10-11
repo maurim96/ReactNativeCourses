c1bcdd52bc850b86965154ded4f67ac73fca0060
Mauricio <mauricio.mino@overactive.com>
2019-10-11 15:30:08 -0300

    Added json-server library (mocked server) in order to rewrite the Blog app and start working with htpp requests. Blog app udpated, currently working with local json-server

---
+++ blog/src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "http://dc81cc73.ngrok.io"
+});
--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,26 +1,34 @@
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
     switch (action.type) {
-        case "add_blogpost":
-            return [
-                ...state,
-                { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content }
-            ];
+        // case "add_blogpost":
+        //     return [
+        //         ...state,
+        //         { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content }
+        //     ];
         case "delete_blogpost":
             return state.filter(blogPost => blogPost.id !== action.payload);
         case "edit_blogpost":
             return state.map(blogPost => {
                 return blogPost.id === action.payload.id ? action.payload : blogPost;
             });
+        case "get_blogposts":
+            return action.payload;
         default:
             return state;
     }
 };
 
 const addBlogPost = dispatch => {
-    return (title, content, callback) => {
-        dispatch({ type: "add_blogpost", payload: { title, content } });
+    return async (title, content, callback) => {
+        // dispatch({ type: "add_blogpost", payload: { title, content } });
+        try {
+            await jsonServer.post("/blogposts", { title, content });
+        } catch (error) {
+            // delete the "added" blogpost on client side
+        }
         if (callback) {
             callback();
         }
@@ -28,20 +36,40 @@ const addBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-    return id => {
+    return async id => {
         dispatch({ type: "delete_blogpost", payload: id });
+        try {
+            await jsonServer.delete(`/blogposts/${id}`);
+        } catch (error) {
+            // add again the "deleted" blogpost on client side
+        }
     };
 };
 
 const editBlogPost = disptach => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
         disptach({ type: "edit_blogpost", payload: { id, title, content } });
+
+        try {
+            await jsonServer.put(`blogposts/${id}`, { title, content });
+        } catch (error) {
+            // rollback the "updated" blogpost on client side
+        }
         if (callback) {
             callback();
         }
     };
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [
-    { id: 1, title: "First Blog Post", content: "Mocked Blog Post" }
-]);
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonServer.get("/blogposts");
+        dispatch({ type: "get_blogposts", payload: response.data });
+    };
+};
+
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+    []
+);
